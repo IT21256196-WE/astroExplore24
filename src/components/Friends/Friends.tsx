94184c1aa481c85c50eb5e020c97bb248e4de14b
Nethmi <it21256196@my.sliit.lk>
2024-05-12 08:53:09 +0530

    files deleted

--- src/components/Friends/Friends.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function Friends() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-        <Typography variant="h2" fontWeight={500}>Friends</Typography>
-    </div>
-  )
-}
--- src/utils/Components.jsx
+++
@@ -1,7 +0,0 @@
-import Navbar from "../components/Navbar/Navbar";
-import Hero from "../components/Hero/Hero";
-import About from "../components/About/About";
-import SpaceNews from "../components/SpaceNews/SpaceNews"
-import Testimonials from "../components/Testimonials/Testimonials";
-import Footer from "../components/Footer/Footer";
-export { Navbar, Hero, About, SpaceNews, Testimonials, Footer };    
